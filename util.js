6ee2c6f3c9dca756cecea3a8b88437cb14aac8aa
ASHISH PRAKASH <60266415+Ashish-Prakash@users.noreply.github.com>
2021-05-26 17:12:24 +0530

    Add files via upload

---
+++ util.js
@@ -0,0 +1,8 @@
+module.exports =
+{
+    "Documents" : ["doc", "pdf", "txt"],
+    "Images" : ["img","jpg", "gif", "png"],
+    "videos" : ["mp4", "mkv"],
+    "audio" : ["mp3"],
+    "application" : ["exe"]
+}
